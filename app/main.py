b00948bd82ffa5505f351c5ca2d17a772e53521a
Mark Milburn <mark.a.milburn@gmail.com>
2025-01-05 17:26:48 -0700

    Handle exit codes #cn8

--- app/main.py
+++ app/main.py
@@ -8,5 +8,6 @@ def main():
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode(), end="")
     print(completed_process.stderr.decode(), file=sys.stderr, end="")
+    exit(completed_process.returncode)
 if __name__ == "__main__":
     main()
