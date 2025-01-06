3b3fe9e5a15f5d16ae04f897dbba52512246a737
Mark Milburn <mark.a.milburn@gmail.com>
2025-01-05 17:24:30 -0700

    Wireup stdout & stderr #kf3

--- app/main.py
+++ app/main.py
@@ -6,8 +6,7 @@ def main():
     command = sys.argv[3]
     args = sys.argv[4:]
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode())
-
-
+    print(completed_process.stdout.decode(), end="")
+    print(completed_process.stderr.decode(), file=sys.stderr, end="")
 if __name__ == "__main__":
     main()
