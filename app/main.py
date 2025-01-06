a35acd8528af1b7dfc038ba266aad9b0351d6aff
Mark Milburn <mark.a.milburn@gmail.com>
2025-01-05 18:01:44 -0700

    Filesystem isolation #if6

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
+import os
 import subprocess
 import sys
+from shutil import copy
+from tempfile import TemporaryDirectory
+from pathlib import Path
 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode(), end="")
-    print(completed_process.stderr.decode(), file=sys.stderr, end="")
-    exit(completed_process.returncode)
+    with TemporaryDirectory() as temp_dir:
+        copy(command, temp_dir)
+        os.chroot(temp_dir)
+        root = Path("/")
+        command = root / Path(command).name
+        completed_process = subprocess.run([command, *args], capture_output=True)
+        print(completed_process.stdout.decode(), end="")
+        print(completed_process.stderr.decode(), file=sys.stderr, end="")
+        exit(completed_process.returncode)
+
+
 if __name__ == "__main__":
     main()
